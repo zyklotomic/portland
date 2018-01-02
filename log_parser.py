41d979b25684d7b665de6265999f92e6591a15ec
Ethan Kiang <chocopuff298@gmail.com>
2018-01-02 23:22:04 +0800

    Created log_parser.py

--- log_parser.py
+++ log_parser.py
@@ -7,4 +7,6 @@ def sec_to_time(seconds): # [days, hours, minutes, seconds]
     d, h = divmod(h, 24) 
     return [d, h, m, s]
 
-
+def parseLog(log_dir=emerge_log_dir,  buff_size=8192):
+    pass
+    
